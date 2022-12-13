2317ff5a278af5f956eca81c9d1ad2fafcabfd41
Arron <arronmurray290104"gmail.com>
2022-12-13 11:44:43 +0000

    added completed and not completed area

--- todoPage.php
+++ todoPage.php
@@ -27,39 +27,64 @@
         </form>
     </div>
 
+    <h2>Not Completed</h2>
     <?php
-        $getTodos = "SELECT todo FROM `todos` WHERE `userid` = '$id';";
-        $allTodos = mysqli_query($con, $getTodos);
-        $finalTodos = mysqli_fetch_all($allTodos);
+        $getTodosNotCompleted = "SELECT todo FROM `todos` WHERE `userid` = '$id' AND `completed` = 0;";
+        $allTodosNotCompleted = mysqli_query($con, $getTodosNotCompleted);
+        $finalTodosNotCompleted = mysqli_fetch_all($allTodosNotCompleted);
+
+        $getTodosCompleted = "SELECT todo FROM `todos` WHERE `userid` = '$id' AND `completed` = 1;";
+        $allTodosCompleted = mysqli_query($con, $getTodosCompleted);
+        $finalTodosCompleted = mysqli_fetch_all($allTodosCompleted);
 
         $getCompletedValues = "SELECT `completed` FROM `todos` WHERE `userid` = '$id';";;
         $allCompleted = mysqli_query($con, $getCompletedValues);
         $finalCompleted = mysqli_fetch_all($allCompleted);
-        // $finalTodos[$x][0]
         
-        for ($x = 0; $x < count($finalTodos); $x++): ?>
+        
+        for ($x = 0; $x < count($finalTodosNotCompleted); $x++): ?>
+            
             <div style = "margin-bottom: 20px;">
                 <form action = "changeStatus.php" method = "post" style = "display: inline-block;">
-                    <input type="hidden" name = "todo_name" value = "<?php echo $finalTodos[$x][0] ?>">
-                    <input type = 'checkbox' <?php echo $finalCompleted[$x][0] ? 'checked' : '' ?>>
+                    <input type="hidden" name = "todo_name" value = "<?php echo $finalTodosNotCompleted[$x][0] ?>">
+                    <input type = 'checkbox'>
                 </form>
 
-                <?php echo $finalTodos[$x][0]; ?>
+                <?php echo $finalTodosNotCompleted[$x][0]; ?>
 
                 <form action = "delete.php" method="post" style="display: inline-block;">
-                    <input type="hidden" name = "todo_name" value = "<?php echo $finalTodos[$x][0] ?>">
+                    <input type="hidden" name = "todo_name" value = "<?php echo $finalTodosNotCompleted[$x][0] ?>">
                     <button>Delete</button>
                 </form>
 
                 <form action = "edit.php" method="post" style="display: inline-block;">
-                    <input type="hidden" name = "todo_name" value = "<?php echo $finalTodos[$x][0] ?>">
+                    <input type="hidden" name = "todo_name" value = "<?php echo $finalTodosNotCompleted[$x][0] ?>">
                     <input type="text" name = "updatedTodo" placeholder = 'Updated Todo'>
                     <button>Update</button>
                 </form>
 
             </div>
 
-    <?php endfor ?>  
+        <?php endfor ?> 
+        
+        <h2>Completed</h2>
+        <?php
+            for ($x = 0; $x < count($finalTodosCompleted); $x++): ?>
+            <div style = "margin-bottom: 20px;">
+                <form action = "changeStatus.php" method = "post" style = "display: inline-block;">
+                    <input type="hidden" name = "todo_name" value = "<?php echo $finalTodosCompleted[$x][0] ?>">
+                    <input type = 'checkbox' checked="checked">
+                </form>
+
+                <?php echo $finalTodosCompleted[$x][0]; ?>
+
+                <form action = "delete.php" method="post" style="display: inline-block;">
+                    <input type="hidden" name = "todo_name" value = "<?php echo $finalTodosCompleted[$x][0] ?>">
+                    <button>Delete</button>
+                </form>
+            </div>
+        
+        <?php endfor ?> 
 
     <script>
         const checkboxes = document.querySelectorAll('input[type=checkbox]')
